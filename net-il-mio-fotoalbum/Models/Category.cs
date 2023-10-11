0866dba5e521b5beec536eacbc415db17cb9b40b
leonardoserra <serra.leonardo92@gmail.com>
2023-10-11 14:42:59 +0200

    creati modelli per Photo e Category con relative proprietà e relazione n to n

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("categories")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Column("title")]
+        [Required]
+        [StringLength(100,ErrorMessage ="Massimo 100 caratteri per titolo categoria")]
+        public string Title { get; set; }
+
+
+        //relation n to n
+        [Column("photos")]
+        List<Photo>? Photos { get; set; }
+
+
+        //constructor 
+        public Category() { }   
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("photos")]
+    public class Photo
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+
+        [Column("title")]
+        [Required]
+        [StringLength(100, ErrorMessage="Massimo 100 caratteri nel titolo della foto.")]
+        public string Title { get; set; }
+
+
+        [Column("description")]
+        [StringLength(500, ErrorMessage = "Massimo 500 caratteri nella descrizione della foto.")]
+        public string Description { get; set; } = "";
+
+
+        [Column("image_file")]
+        [Required]
+        public byte[] ImageFile { get; set; } 
+
+
+        [Column("visibility")]
+        public bool Visibility { get; set; } = true;
+
+
+        //relation n to n
+        [Column("categories")]
+        List<Category>? Categories { get; set; }
+
+
+        //constructor 
+        public Photo() { }
+    }
+}
