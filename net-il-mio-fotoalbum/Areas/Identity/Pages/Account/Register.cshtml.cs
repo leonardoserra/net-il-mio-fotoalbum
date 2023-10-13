d1c4e5cecc0a6fccceb3e8c972cdd7caa5333b03
leonardoserra <serra.leonardo92@gmail.com>
2023-10-13 11:10:34 +0200

    aggiunto defaultRole USER quando ci si iscrive

--- net-il-mio-fotoalbum/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ net-il-mio-fotoalbum/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -29,13 +29,15 @@ namespace net_il_mio_fotoalbum.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<IdentityUser> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             IUserStore<IdentityUser> userStore,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+             RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -43,6 +45,7 @@ namespace net_il_mio_fotoalbum.Areas.Identity.Pages.Account
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _roleManager = roleManager;
         }
 
         /// <summary>
@@ -113,7 +116,12 @@ namespace net_il_mio_fotoalbum.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+                var defaultrole = _roleManager.FindByNameAsync("USER").Result;
 
+                if (defaultrole != null)
+                {
+                    IdentityResult roleresult = await _userManager.AddToRoleAsync(user, defaultrole.Name);
+                }
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
